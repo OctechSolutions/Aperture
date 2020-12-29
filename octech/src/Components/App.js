0e7dcf7eb8cd6458b4debb8198ab61ca850fc641
ggn1 <ggn1@hw.ac.uk>
2020-12-29 16:13:06 +0400

    Blank Started Code

--- octech/src/Components/App.js
+++ octech/src/Components/App.js
@@ -3,13 +3,7 @@ import logo_beige from "../images/aperture_lens_beige.png"
 
 class App extends React.Component{
   render(){
-    return (
-      <div className="placeholder">
-        <img id="logo_image" src={logo_beige} alt="aperture lens logo" />
-        <div className="break"></div>
-        <h1 id="logo_writing">Aperture</h1>
-      </div>
-    )
+    return (<h1>Aperture</h1>)
   }
 }
 
