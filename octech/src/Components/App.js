c62d17f452942a42e595ac35c18aa5abd96aa27e
ggn1 <ggn1@hw.ac.uk>
2020-12-29 16:05:26 +0400

    Small change

--- octech/src/Components/App.js
+++ octech/src/Components/App.js
@@ -1,11 +1,11 @@
 import React from "react"
-import logo_yellow from "../images/aperture_lens_beige.png"
+import logo_beige from "../images/aperture_lens_beige.png"
 
 class App extends React.Component{
   render(){
     return (
       <div className="placeholder">
-        <img id="logo_image" src={logo_yellow} alt="aperture lens logo" />
+        <img id="logo_image" src={logo_beige} alt="aperture lens logo" />
         <div className="break"></div>
         <h1 id="logo_writing">Aperture</h1>
       </div>
