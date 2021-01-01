1839e891de9ded5dbc31e46a6d6901fd0408a644
ggn1 <ggn1@hw.ac.uk>
2021-01-01 12:08:07 +0400

    Made Login front end responsive

--- octech/src/index.js
+++ octech/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './Components/App'
+import storage from './Components/Firebase'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
