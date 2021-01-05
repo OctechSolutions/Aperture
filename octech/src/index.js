0d213470a88dda1aa499b06d7416e7f6f5bcf2e3
ggn1 <ggn1@hw.ac.uk>
2021-01-05 11:24:25 +0400

    Removed faulty code

--- octech/src/index.js
+++ octech/src/index.js
@@ -2,6 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './Components/App'
-import storage from './Components/Firebase'
 
 ReactDOM.render(<App />, document.getElementById('root'))
