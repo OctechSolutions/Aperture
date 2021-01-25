f8761c5e51da23728c3588840ea3556cd12ed4fa
ggn1 <ggn1@hw.ac.uk>
2021-01-25 22:30:09 +0400

    Started Portfolio

--- octech/src/components/Body/Portfolio/Portfolio.js
+++ octech/src/components/Body/Portfolio/Portfolio.js
@@ -1,36 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { db, auth } from "../../../firebase"
 import './Portfolio.css'
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden',
-      backgroundColor: theme.palette.background.paper,
-    },
-    gridList: {
-      width: 500,
-      height: 450,
-    },
-}));
+import '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 export default function Portfolio() {
-    const classes = useStyles();
-    const tileData = [];
-    return (
-        <div className={classes.root}>
-            <h3>Portfolio!</h3>
-            <GridList cellHeight={160} className={classes.gridList} cols={3}>
-            {tileData.map((tile) => (
-                <GridListTile key={tile.img} cols={tile.cols || 1}>
-                <img src={tile.img} alt={tile.title} />
-                </GridListTile>
-            ))}
-            </GridList>
-        </div>
-    )
+    const portfolioRef =db.collection("portfolios")
+
+    const [hasPortfolio, setHasPortfolio] = useState(false)
+
+    /* Method that will set the value of state hasPortfolio to 
+       true if the current user alredy has a portfolio and 
+       will set it to false otherwise. */
+    const checkIfPortfolioExists = () => {
+        portfolioRef.where("creator", "==", auth.currentUser.displayName).get()
+        .then( (userPortfolio) => { 
+            console.log(userPortfolio.data)
+            if(userPortfolio.data && hasPortfolio != true) {
+                setHasPortfolio(true)
+            } 
+        })
+    }
+
+    useEffect(() => {checkIfPortfolioExists()}, [hasPortfolio])
+
+    if(hasPortfolio) {
+        return (
+            <div className="portfolio">
+                <p>Awesome! You have a portfolio!</p>
+                <button className="edit-portfolio-btn btn btn-dark">
+                    Edit
+                </button>
+            </div>
+        )
+    } else {
+        return (
+            <div className="no-portfolio">
+                <p>Looks like your still on the hunt for the perfect portfolio!</p>
+                <button className="create-portfolio-btn btn btn-dark">
+                    I'm Ready! Lets Make One Now!
+                </button>
+            </div>
+        )
+    }
 }
