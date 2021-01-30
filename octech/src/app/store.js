afe7ceda8af288b34eb3cde26de3714b14848ab2
Gayathri Girish Nair <55646435+ggn1@users.noreply.github.com>
2021-01-30 23:16:46 +0400

    Deleted Octech directory

--- octech/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../features/userSlice';
-
-// Redux wrapper with a variable of user set by userReducer
-export default configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
--- octech/src/components/Body/Map/client-config.js
+++
@@ -1,1 +0,0 @@
-export const GoogleMapsAPI = 'AIzaSyC1EfNasAc6J8vIFP3Lephiv4sKQwFmvFQ';
