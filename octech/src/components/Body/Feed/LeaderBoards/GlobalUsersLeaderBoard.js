c12f2d00676dbe2941ae703d802a9213a98d05f4
mme11 <55647677+mme11@users.noreply.github.com>
2021-03-12 18:45:28 +0400

    Fixed the issue where leaderboards call the db infinitely

--- octech/src/components/Body/Feed/LeaderBoards/GlobalUsersLeaderBoard.js
+++ octech/src/components/Body/Feed/LeaderBoards/GlobalUsersLeaderBoard.js
@@ -6,18 +6,20 @@ import LeaderBoardComponent from './LeaderBoardComponent';
 
 function GlobalUsersLeaderBoard({ match, setValue }) {
   const [leaderboardData, setLeaderBoardData] = useState([]);
+  const [dataFetched, setDataFetched] = useState(false);
 
-  useEffect(() => {
-    db.collection('users')
-      .orderBy('profilePoints', 'desc')
-      .limit(100)
-      .onSnapshot(data => {
-        setLeaderBoardData(data.docs.map(user => {
-          return user.data();
-        }));
-      })
+    if (dataFetched===false) {
+      db.collection('users')
+        .orderBy('profilePoints', 'desc')
+        .limit(100)
+        .onSnapshot(data => {
+          setDataFetched(true);
+          setLeaderBoardData(data.docs.map(user => {
+            return user.data();
+          }));
+        })
+    }
 
-  })
   return (
     <LeaderBoardComponent title="Global Users LeaderBoard" headers={['Name','Profile Points']} columns={['name', 'profilePoints']} data={leaderboardData} />
   )
