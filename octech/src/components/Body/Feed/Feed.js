07768c7f0dc6b093fe47d3501a19835fa93c2f48
Gaurav-Gosain <55647468+Gaurav-Gosain@users.noreply.github.com>
2021-03-13 13:52:51 +0400

    fixed css for Home title

--- octech/src/components/Body/Feed/Feed.js
+++ octech/src/components/Body/Feed/Feed.js
@@ -680,7 +680,7 @@ function Feed({ match }, props) {
             </div>
           </>
           :
-          <div style={{ position: "sticky", top: "65px", zIndex: "100", backgroundColor: "whitesmoke", width: "105vw", marginLeft: "-5vw" }}>
+          <div style={{ position: "sticky", top: "67px", zIndex: "100", backgroundColor: "whitesmoke", width: "104%",marginLeft: "-2%",overflow: "hidden"}}>
             <center>
               <h1>Home</h1>
             </center>
