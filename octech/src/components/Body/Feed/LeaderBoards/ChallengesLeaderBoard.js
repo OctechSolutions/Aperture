34706800ea116039ae95537508f1e9e8a0e75259
mme11 <55647677+mme11@users.noreply.github.com>
2021-03-15 02:27:57 +0400

    Challenge Leaderboard

--- octech/src/components/Body/Feed/LeaderBoards/ChallengesLeaderBoard.js
+++ octech/src/components/Body/Feed/LeaderBoards/ChallengesLeaderBoard.js
@@ -1,12 +1,30 @@
-import React from 'react';
-// import {db} from '../../../../firebase';
-// import firebase from 'firebase';
+import React, { useState } from 'react';
+import { db } from '../../../../firebase';
 import LeaderBoardComponent from './LeaderBoardComponent';
 
-function ChallengesLeaderBoard({ match, setValue }) {
+
+function ChallengesLeaderBoard() {
+
+  const [leaderboardData, setLeaderBoardData] = useState([]);
+  const [dataFetched, setDataFetched] = useState(false);
+
+  if (dataFetched === false) {
+
+    db.collection('challengePosts')
+      .orderBy('totalStars', 'desc')
+      .limit(100)
+      .onSnapshot(data => {
+        setDataFetched(true);
+        setLeaderBoardData(data.docs.map(post => {
+          return post.data();
+        }));
+      })
+    }
     return (
-        <LeaderBoardComponent title="Challenges LeaderBoard" headers={['Name', 'Points']} columns={[]} data={[]}/>
+
+      <LeaderBoardComponent title="Challenges LeaderBoard" headers={['Post Caption', 'Submitted By', 'Total Stars']} columns={['caption','creator','totalStars']} data={leaderboardData} />
+
     )
-};
+  };
 
 export default ChallengesLeaderBoard;
--- octech/src/components/Body/Feed/LeaderBoards/GlobalPostsLeaderBoard.js
+++ octech/src/components/Body/Feed/LeaderBoards/GlobalPostsLeaderBoard.js
@@ -4,24 +4,29 @@ import LeaderBoardComponent from './LeaderBoardComponent';
 
 
 function GlobalPostsLeaderBoard() {
+
   const [leaderboardData, setLeaderBoardData] = useState([]);
   const [dataFetched, setDataFetched] = useState(false);
 
-  if (dataFetched===false) {
+  if (dataFetched === false) {
+
     db.collection('posts')
-    .orderBy('totalStars', 'desc')
-    .limit(10)
-    .onSnapshot(data => {
-      setDataFetched(true);
-      setLeaderBoardData(data.docs.map(post => {
-        return post.data();
-      }));
-    })
+      .orderBy('totalStars', 'desc')
+      .limit(10)
+      .onSnapshot(data => {
+        setDataFetched(true);
+        setLeaderBoardData(data.docs.map(post => {
+          return post.data();
+
+        }));
+      })
   }
-    
+
 
   return (
+
     <LeaderBoardComponent title="Global Posts LeaderBoard" headers={['Post Caption', 'Posted By', 'Total Stars']} columns={['message', 'name', 'totalStars']} data={leaderboardData} />
+
   )
 }
 
